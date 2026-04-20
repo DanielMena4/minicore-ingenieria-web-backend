ab9644962fc7a987f99c3ef578a27bec4d2e2a12
Danielmena4 <daniel__mena@hotmail.com>
2026-04-19 22:27:58 -0500

    deploy

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const app = express();
-app.use(cors({
-    origin: '*'
-}));
+
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 app.get('/comisiones', async (req, res) => {
@@ -21,18 +23,18 @@ app.get('/comisiones', async (req, res) => {
 
     try {
         const [rows] = await pool.query(`
-        SELECT
-        ven.nombre,
-        v.monto,
-        v.fecha,
-        r.porcentaje,
-        (v.monto * r.porcentaje / 100) AS comision
-        FROM ventas v
-        JOIN vendedor ven ON ven.id = v.vendedor_id
-        JOIN regla r
-        ON v.monto BETWEEN r.monto_min AND r.monto_max
-        WHERE v.fecha BETWEEN ? AND ?
-    `, [inicio, fin]);
+            SELECT
+                ven.nombre,
+                v.monto,
+                v.fecha,
+                r.porcentaje,
+                (v.monto * r.porcentaje / 100) AS comision
+            FROM ventas v
+            JOIN vendedor ven ON ven.id = v.vendedor_id
+            JOIN regla r
+                ON v.monto BETWEEN r.monto_min AND r.monto_max
+            WHERE v.fecha BETWEEN ? AND ?
+        `, [inicio, fin]);
 
         res.json(rows);
     } catch (error) {
@@ -41,6 +43,8 @@ app.get('/comisiones', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log('Servidor corriendo en puerto', PORT);
 });
